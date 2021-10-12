c7cb914af340421d4ce4d0b9c976cd45dfb3920f
Dustin MC <DustinMenjivarCornejo@gmail.com>
2021-10-11 22:31:18 -0600

    Test. PWA code added

--- Scripts/BaseScripts.js
+++ Scripts/BaseScripts.js
@@ -3,6 +3,14 @@ let $nav= document.querySelectorAll(".nav");
 let $menuButton= document.querySelectorAll(".menuButton");
 
 /* Functions scope */{
+ /* ServiceWorker */
+ if("serviceWorker" in navigator){
+  navigator.serviceWorker.register("./ServiceWorker_En.js")
+  .then(reg => console.info("ServiceWorked registered", reg))
+  .catch(err => console.warn("Error during ServiceWorked registry", err))
+ }
+ else{ console.info("No ServiceWorker available \n(js scope line07)"); }
+
  /* Scroll up the page */
  function ScrollUp(){
   let $documentScroll= document.documentElement,
@@ -10,7 +18,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
   if($documentScroll.scrollTop>0){$documentScroll.scrollTo(0, 0);}
   else if($mainScroll.scrollTop> 0){$mainScroll.scrollTo(0, 0);}
  }
-
+ 
  /* gets true (show) or false (hide) */
  function ShowHideSpiner(show){
   let $spiner= document.querySelector(".container-spiner");
@@ -25,28 +33,30 @@ let $menuButton= document.querySelectorAll(".menuButton");
    }, 500);
   }
  }
-
+ 
  /* setting buttons-copy */
  document.querySelectorAll(".copyableElement__button").forEach($copyButton => {
-  $copyButton.onclick= function(){
-   CopyElement(this.parentElement);
-   /* setTimeout(() => { this.innerText= "Copy"; }, 1000); */
-  }
+  $copyButton.onclick= function(){ CopyElement(this.parentElement); }
  });
  function CopyElement($copyableElementContainer){
-  /* Selecting the value */
+  /* Selecting elements */
   let $copyableItem= $copyableElementContainer.querySelector(".copyableElement"),
   $copyButton= $copyableElementContainer.querySelector(".copyableElement__button");
-  $copyableItem.select();
-  $copyableItem.setSelectionRange(0, -1); /* For mobile devices */;
   /* Copying the value */
-  navigator.clipboard.writeText($copyableItem.value);
-  console.log("copied element");
+  try { navigator.clipboard.writeText($copyableItem.value); }
+  catch (error) {
+   /* For dont supported navigator.clipboard */
+   console.info("Copied with no standard code \n(js scope line44), because:", error);
+   $copyableItem.classList.replace("display-n", "display-y");
+   $copyableItem.select();
+   document.execCommand("copy");
+   $copyableItem.classList.replace("display-y", "display-n");
+  }
   /* Showing that the value has been copied */
   $copyButton.innerText= "Copied";
   setTimeout(() => { $copyButton.innerText= "Copy"; }, 1000);
  }
-
+ 
  /* GenericElement button, hider data */
  document.querySelectorAll(".genericElement__button").forEach(button => {
   button.onclick= function(){
@@ -54,11 +64,11 @@ let $menuButton= document.querySelectorAll(".menuButton");
    this.parentElement.querySelector(".genericElement__data").classList.toggle("display-y");
   }
  });
-
+ 
  /* Create course section Src or SrcSet  */
  function CreateSrcOrSrcSet(dirName, type){
   let value= "./Sources/Images/MainSection_Courses/";
-
+  
   /* If type== true then, we create the src value */
   if(type){
    value+= dirName +"/" +dirName +"0480px.png";
@@ -91,62 +101,54 @@ let $menuButton= document.querySelectorAll(".menuButton");
   $nav[1].classList.replace("display-n", "display-y");
   $menuButton[0].classList.replace("display-y", "display-n")
  }
-
+ 
  /* Vp resized. show menus cheker */
  window.addEventListener("resize", function(){
   if(document.documentElement.clientWidth<960){
    $nav[0].classList.replace("display-y", "display-n");
    $nav[1].classList.replace("display-y", "display-n");
-   if (isHomeSection) {
-    $menuButton[0].classList.replace("display-n", "display-y");
-   }
-   else{
-    $menuButton[1].classList.replace("display-n", "display-y");
-   }
+   if (isHomeSection) { $menuButton[0].classList.replace("display-n", "display-y"); }
+   else{ $menuButton[1].classList.replace("display-n", "display-y"); }
   }
   if(document.documentElement.clientWidth>=960){
    $nav[1].classList.replace("display-n", "display-y");
    $nav[0].classList.replace("display-y", "display-n");
-   if (isHomeSection) {
-    $menuButton[0].classList.replace("display-y", "display-n");
-   }
-   else{
-    $menuButton[1].classList.replace("display-y", "display-n");
-   }
+   if (isHomeSection) { $menuButton[0].classList.replace("display-y", "display-n"); }
+   else{ $menuButton[1].classList.replace("display-y", "display-n"); }
   }
  });
-
- /* Menu buttons toggler */
+ 
+ /* Menu buttons visibility toggler (mobile buttons) */
  function ShowMenuButton(x){
-    if(document.documentElement.clientWidth<960){
-      $nav[1].classList.replace("display-y", "display-n");
-      if(x){
-        $menuButton[0].classList.replace("display-n", "display-y");
-        $menuButton[1].classList.replace("display-y", "display-n");
-      }
-      else{
-        $menuButton[0].classList.replace("display-y", "display-n");
-        $menuButton[1].classList.replace("display-n", "display-y");
-      }
-    }
+  if(document.documentElement.clientWidth<960){
+   $nav[1].classList.replace("display-y", "display-n");
+   if(x){
+    $menuButton[0].classList.replace("display-n", "display-y");
+    $menuButton[1].classList.replace("display-y", "display-n");
+   }
+   else{
+    $menuButton[0].classList.replace("display-y", "display-n");
+    $menuButton[1].classList.replace("display-n", "display-y");
+   }
+  }
  }
-
+ 
  /* Menu visibility toglers */
  for (let i = 0; i < 2; i++) {
-    $menuButton[i].onclick= function(){
-      $nav[i].classList.toggle("display-n");
-      $nav[i].classList.toggle("display-y");
-    }
-    document.querySelectorAll(".nav__header")[i].onclick= function(){
-      $nav[i].classList.toggle("display-n");
-      $nav[i].classList.toggle("display-y");
-    }
-    document.querySelectorAll(".nav__closeMenu")[i].onclick= function(){
-      $nav[i].classList.toggle("display-n");
-      $nav[i].classList.toggle("display-y");
-    }
+  $menuButton[i].onclick= function(){
+   $nav[i].classList.toggle("display-n");
+   $nav[i].classList.toggle("display-y");
+  }
+  document.querySelectorAll(".nav__header")[i].onclick= function(){
+   $nav[i].classList.toggle("display-n");
+   $nav[i].classList.toggle("display-y");
+  }
+  document.querySelectorAll(".nav__closeMenu")[i].onclick= function(){
+   $nav[i].classList.toggle("display-n");
+   $nav[i].classList.toggle("display-y");
+  }
  }
-
+ 
  /* .Languages toggler */
  let languages= document.querySelectorAll(".nav")[1].querySelectorAll(".nav__item")[3];
  languages.onclick= function(){
@@ -157,7 +159,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
   languages.querySelector(".languages").classList.toggle("display-yi");
   languages.querySelector(".languages").classList.toggle("display-n");
  }
-
+ 
  /* Section toggler buttons (.nav__items) */
  let $menuItem= $nav[1].querySelectorAll(".nav__item");
  $menuItem[0].onclick= function(){
@@ -170,6 +172,8 @@ let $menuButton= document.querySelectorAll(".menuButton");
   $menuItem[i].onclick= function(){
    isHomeSection= false;
    ShowMenuButton(isHomeSection);
+   //when is nav course button
+   if(i==1){ SwitchToCourseSection(0); }
    if (i<3) { SwitchToMainSection(i); }
    else{ SwitchToMainSection(i-1); }
   }
@@ -191,7 +195,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
   }
   ShowHideSpiner(false);
  }
-
+ 
  /* secttionCard button onclick */
  let $sectionCardButton= document.querySelector(".mainSection").querySelectorAll(".sectionCard__button");
  for (let i = 0; i < 4; i++) {
@@ -208,8 +212,8 @@ let $menuButton= document.querySelectorAll(".menuButton");
 
 /* Courses */{
  var currentCourse= -1,
-  currentModule= -1;
-
+ currentModule= -1;
+ 
  function SwitchToCourseSection(index){
   ScrollUp();
   let $courseSection= document.querySelectorAll(".mainSection")[1].querySelectorAll(".mainSection__subSection");
@@ -224,31 +228,31 @@ let $menuButton= document.querySelectorAll(".menuButton");
    }
   }
  }
-
+ 
  function CreateCourseSection(index){
   /* index= courseIndex to be created */
   if(currentCourse!= index){
    currentCourse= index;
    currentModule= -1;
-
+   
    let courseData= CoursesData[currentCourse];
-
+   
    /* Elements to create the content */
    let $template= document.getElementById("template-courseSection").content,
    $courseSection= document.querySelectorAll(".mainSection")[1].querySelectorAll(".mainSection__subSection")[1],
    $fragment= document.createDocumentFragment(),
    $cloned= document.importNode($template, true);
-
+   
    /* Setting image data */
    $cloned.querySelector("img").setAttribute("src", CreateSrcOrSrcSet(courseData.topic, true));
    $cloned.querySelector("img").setAttribute("srcset", CreateSrcOrSrcSet(courseData.topic, false));
    $cloned.querySelector("img").setAttribute("alt", courseData.imageAlt);
    $cloned.querySelector("img").setAttribute("title", courseData.imageTitle);
-
+   
    /* Setting header data */
    $cloned.querySelector(".course__header-title").textContent= courseData.title;
    $cloned.querySelector(".course__header-text").textContent= courseData.descriptionCourse;
-       
+   
    /* Setting course "meta" data */
    $cloned.querySelectorAll(".Kx-icon")[0].textContent= courseData.modules.length;
    $cloned.querySelectorAll(".Kx-icon")[1].textContent= "##:##:##";
@@ -278,7 +282,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
      $list1.appendChild($item);
     }
    });
-
+   
    /* Setting optional requirements data */
    let $list2= $cloned.querySelector(".container").querySelectorAll(".list")[2];
    courseData.optionalRequirements.forEach(element => {
@@ -302,7 +306,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
      $list2.appendChild($item);
     }
    });
-
+   
    /* Setting resources data */
    let $resources= $cloned.querySelectorAll(".resourceItem");
    $resources[0].setAttribute("download", courseData.manualName +".pdf");
@@ -312,7 +316,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
    $resources[1].setAttribute("href", "../Sources/Manuals/English/" +courseData.manualName +".pdf");
    $resources[2].setAttribute("download", courseData.manualName +"-exercices.pdf");
    $resources[2].setAttribute("href", "../Sources/Manuals/English/" +courseData.manualName +".pdf"); */
-
+   
    /* Setting modules data */
    let $modulesList= $cloned.querySelector(".courseModules");
    $modulesList.querySelector(".Kx-icon").classList.add(courseData.iconName);
@@ -323,7 +327,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
     $moduleImg= document.createElement("img"),
     $moduleDiv= document.createElement("div"),
     $span= document.createElement("span");
-
+    
     $item.classList.add("courseModules__module");
     $item.onclick= function(){
      CreateModuleSection(i);
@@ -332,7 +336,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
      }
      SwitchToCourseSection(2);
     };
-
+    
     /* Setting img data */
     $moduleImg.classList.add("courseModules__module-image");
     $moduleImg.setAttribute("alt", "Module image")
@@ -344,57 +348,57 @@ let $menuButton= document.querySelectorAll(".menuButton");
      $moduleImg.setAttribute("src", "./Sources/Images/MainSection_Courses/" +courseData.topic +"/Modules/"  + "Module" + i +".png");
      $moduleImg.setAttribute("srcset", "./Sources/Images/MainSection_Courses/" +courseData.topic +"/Modules/"  + "Module" + i +".webp");
     }
-
+    
     /* Setting module name */
     $moduleDiv.innerText= $element.title;
-
+    
     /* Setting span data */
     $span.classList.add("Kx-icon", "Kx-play-button");
     $span.innerText= $element.duration;
-
+    
     /* Adding the content to the module */
     $item.appendChild($moduleImg);
     $moduleDiv.appendChild($span);
     $item.appendChild($moduleDiv);
     $modulesList.appendChild($item);
    }
-
+   
    /* Adding the content to the document */
    $fragment.appendChild($cloned);
    $courseSection.innerHTML="";
    $courseSection.appendChild($fragment);
   }
  }
-
+ 
  function CreateModuleSection(currentModuleIndex){
   if(!(currentModule== currentModuleIndex)){
    currentModule= currentModuleIndex;
    
    let moduleData= CoursesData[currentCourse].modules[currentModuleIndex];
-
+   
    /* Elements to create the content */
    let $template= document.getElementById("template-moduleSection").content,
    $moduleSection= document.querySelectorAll(".mainSection")[1].querySelectorAll(".mainSection__subSection")[2],
    $fragment= document.createDocumentFragment(),
    $cloned= document.importNode($template, true);
-
+   
    /* Setting video data */
    $cloned.querySelector(".container-video__video").setAttribute("src", "https://www.youtube.com/embed/" +moduleData.youtubeId +"?controls=0&rel=0&disablekb=0&enablejsapi=1");
-
+   
    /* Setting header data */
    $cloned.querySelector(".module__header-title").textContent= moduleData.title;
    $cloned.querySelector(".module__header-text").textContent= moduleData.description;
-
+   
    /* Setting module "meta" data */
    $cloned.querySelectorAll(".Kx-icon")[0].textContent= moduleData.content.length;
    $cloned.querySelectorAll(".Kx-icon")[1].textContent= moduleData.duration;
    $cloned.querySelectorAll(".Kx-icon")[2].textContent= moduleData.lastUpdate;
    $cloned.querySelectorAll(".Kx-icon")[3].textContent= moduleData.release;
-
+   
    /* Setting module content (temary) */
    let $moduleContent= $cloned.querySelector(".moduleContentList"),
    $contentFragment= document.createDocumentFragment();
-
+   
    /* Traversing module themes */
    moduleData.content.forEach(moduleTheme => {
     let $itemTheme= document.createElement("li"),
@@ -415,7 +419,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
     /* setting module nested themes */
     else{
      let $nestedThemes= document.createElement("ul");
-
+     
      moduleTheme[1].forEach(nestedModuleTheme => {
       let $nestedItemTheme= document.createElement("li"),
       $nestedItemSpan= document.createElement("span");
@@ -428,30 +432,30 @@ let $menuButton= document.querySelectorAll(".menuButton");
        ScrollUp();
        player01.seekTo(ModuleTime(nestedModuleTheme[1]));
       }
-
+      
       $nestedThemes.appendChild($nestedItemTheme)
      });
-
+     
      $itemTheme.style.setProperty("flex-direction", "column");
      $itemTheme.style.setProperty("padding-right", "0");
      $itemTheme.appendChild($nestedThemes);
     }
-
+    
     $moduleContent.appendChild($itemTheme);
    });
    $moduleContent.appendChild($contentFragment);
-
+   
    /* Adding the content to the document */
    $fragment.appendChild($cloned);
    $moduleSection.innerHTML="";
    $moduleSection.appendChild($fragment);
   }
  }
-
+ 
  function ModuleTime(x){
   let preTime= x.split(":"),
   time;
-
+  
   preTime.reverse();
   time= parseInt(preTime[0])+ (parseInt(preTime[1])*60);
   if( preTime[2]!=undefined){ time+= (parseInt(preTime[2])*3600);}
@@ -479,7 +483,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
 
 /* Youtube videos scope */{
  var player01, player02;
-
+ 
  function onYouTubeIframeAPIReady() {
   player01= true;
   player02 = new YT.Player('player-aboutUs', {
@@ -488,7 +492,7 @@ let $menuButton= document.querySelectorAll(".menuButton");
    }
   });
  }
-
+ 
  function CreateYoutubePlayer01(){
   player01 = new YT.Player('player-module', {
    events: {
@@ -496,16 +500,24 @@ let $menuButton= document.querySelectorAll(".menuButton");
    }
   });
  }
-
+ 
  function onPlayerStateChange(event) {
-  switch (event.data) {
+  switch (event.data){
    case 0: //0== video ended
-    event.target.stopVideo();
-    break;
-
-   case 1: //1== video play
-    //stop the other player
-    break;
+   event.target.stopVideo();
+   break;
+   
+   case 1: //1== video played
+   let $playerId= event.target.getIframe().getAttribute("id");
+   if ($playerId== "player-module" && player02.getPlayerState()==1) {
+    player02.stopVideo();
+   }
+   else{
+    try { //try because sometimes player01 still not created
+     if ($playerId== "player-aboutUs" && player01.getPlayerState()==1) { player01.stopVideo(); }
+    } catch (error) { console.info("Error catched \n(js scope line519):", error); }
+   }
+   break;
   }
  }
 }
---
+++ Scripts/ServiceWorker_En.js
@@ -0,0 +1,61 @@
+const CACHE= "KixneWeb_cache_V0",
+urlsToCache=[
+ "/index.html",
+ "/Sources/Styles/CustomReset.css",
+ "/Sources/Styles/BaseStyles.css",
+ "/Sources/Styles/StylesSmall.css",
+ "/Sources/Styles/StylesMid.css",
+ "/Sources/Fonts/Nunito-Regular.ttf",
+ "/Sources/Fonts/Aladin-Regular.ttf",
+ "/Sources/Icons/KxIcons-v1.0/fonts/KxIcons.svg",
+ "/Sources/Icons/KxIcons-v1.0/KxIcons.css",
+ "/Sources/Icons/KxIcons-v1.0/KxIcons.css",
+ "/Sources/Icons/KxIcons-v1.0/selection.json",
+ "/Scripts/BaseScripts.js",
+ "/Scripts/CoursesDataEn.js",
+ "https://www.youtube.com/iframe_api",
+]
+
+self.addEventListener("install", e=>{
+ e.waitUntil(
+  caches.open(CACHE)
+  .then(cache=>{
+   return cache.addAll(urlsToCache)
+   .then(()=> self.skipWaiting())
+  })
+  .catch(err=> console.log("Error during cache registry", err))
+ )
+})
+
+self.addEventListener("activate", e=>{
+ const cacheWhiteList= [CACHE];
+ e.waitUntil(
+  caches.keys()
+  .then(cacheNames=>{
+   cacheNames.map(cacheName=>{
+    /* Deleting unused cache files */
+    if(cacheWhiteList.indexOf(cacheName)=== -1){
+     return caches.delete(cacheName)
+    }
+   })
+  })
+  /* Activating current cache */
+  .then(()=> self.clients.claim())
+ )
+})
+
+/* When navigator gets an url */
+self.addEventListener("fetch", e=>{
+ /* Answer with cache or searh the real url */
+ e.respondWith(
+  caches.match(e.request)
+  .then(res=>{
+   if(res){
+    /* Recover from cache */
+    return res
+   }
+   /* Recover from the request to the url */
+   return fetch(e.request)
+  })
+ )
+})
